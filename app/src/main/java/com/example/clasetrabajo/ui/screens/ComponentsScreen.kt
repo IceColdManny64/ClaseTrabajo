72f15d44eb6aa4ad497b883b1c7041c438d24924
JOSÉ MANUEL JUÁREZ DÍAZ <22151192@aguascalientes.tecnm.mx>
2025-02-27 19:33:53 -0600

    Added new components (Chips, Progress Bars, Switches, Sliders) to ComponentsScreen

--- app/src/main/java/com/example/clasetrabajo/ui/screens/ComponentsScreen.kt
+++ app/src/main/java/com/example/clasetrabajo/ui/screens/ComponentsScreen.kt
@@ -3,31 +3,60 @@ package com.example.clasetrabajo.ui.screens
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.AssistChip
+import androidx.compose.material3.AssistChipDefaults
 import androidx.compose.material3.Button
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.FilledTonalButton
+import androidx.compose.material3.FilterChip
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.InputChip
+import androidx.compose.material3.InputChipDefaults
 import androidx.compose.material3.LargeFloatingActionButton
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Slider
+import androidx.compose.material3.SliderPositions
 import androidx.compose.material3.SmallFloatingActionButton
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import java.util.logging.Filter
 
 @Composable
 fun ComponentsScreen(navController: NavHostController){
     //Buttons()
-    FloatingButtons()
+    //FloatingButtons()
+    //Progress()
+    //Chips()
+    //Sliders()
+    Switches()
     }
 //@Preview(showBackground = true)
 @Composable
@@ -36,7 +65,7 @@ fun Buttons(){
         modifier = Modifier
             .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.SpaceBetween
+        verticalArrangement = Arrangement.SpaceEvenly
     ){
         //Button styles
         Button(onClick = {}) {
@@ -60,7 +89,7 @@ fun Buttons(){
         }
     }
 }
-@Preview(showBackground = true)
+//@Preview(showBackground = true)
 @Composable
 fun FloatingButtons(){
     Column(
@@ -83,5 +112,170 @@ fun FloatingButtons(){
             Icon(Icons.Filled.Add, contentDescription = "Add Button")
             Text("Button")
         }
+    }
+}
+//@Preview(showBackground = true)
+@Composable
+fun Progress(){
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly
+    ) {
+        LinearProgressIndicator(
+            modifier = Modifier
+                .fillMaxWidth()
+        )
+        CircularProgressIndicator(
+            modifier = Modifier
+                .width(64.dp)
+        )
+    }
+}
+
+//@Preview(showBackground = true)
+@Composable
+fun Chips() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly
+    ) {
+        AssistChip(
+            onClick = {},
+            label = { Text("AssistChip") },
+            leadingIcon = {
+                Icon(
+                Icons.Filled.AccountBox,
+                contentDescription = "Assist Chip",
+                    modifier = Modifier
+                        .size(AssistChipDefaults.IconSize)
+                )
+            }
+        )
+        //mutableStateOf is used to keep an element in a particular state
+        //even if there's changes or updates in the interface
+        var selected by remember { mutableStateOf(false) }
+        FilterChip(
+            //selected defines if the button is pressed or not
+            selected = selected,
+            //This will switch the current state
+            // of FilterChip to the opposite
+            onClick = {selected = !selected},
+            label = {Text("Filter Chip")},
+            leadingIcon = if(selected){
+                {
+                Icon(
+                    Icons.Filled.AccountBox,
+                    contentDescription = "Assist Chip",
+                    modifier = Modifier
+                        .size(AssistChipDefaults.IconSize)
+                )
+            }
+            }else {
+                null
+            }
+        )
+        InputChipExample("Dismiss", {})
+
+    }
+}
+
+@Composable
+fun InputChipExample(
+    text:String,
+    onDismiss: () -> Unit
+){
+    var enabled by remember{ mutableStateOf(true) }
+    if(!enabled) return
+    InputChip(
+        label = {Text(text)},
+        selected = enabled,
+        onClick = {
+            onDismiss()
+            enabled = !enabled
+        },
+        avatar = {
+            Icon(
+                Icons.Filled.Person,
+                contentDescription = "User Icon",
+                Modifier.size(InputChipDefaults.AvatarSize)
+            )
+        },
+        trailingIcon = {
+            Icon(
+                Icons.Filled.Close,
+                contentDescription = "Cross Icon",
+                Modifier.size(InputChipDefaults.AvatarSize)
+            )
+        }
+    )
+}
+//@Preview
+@Composable
+fun Sliders(){
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly
+
+    ){
+        var SliderPosition by remember { mutableStateOf(50f) }
+        Slider(
+            value = SliderPosition,
+            //updates the slider state according to the users interaction with it
+            onValueChange = {SliderPosition = it},
+            steps = 10,
+            valueRange = 0f .. 100f,
+        )
+        Text(
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .fillMaxWidth(),
+            text = SliderPosition.toString()
+        )
+    }
+}
+
+//@Preview
+@Composable
+fun Switches() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly
+
+    ) {
+        var checked by remember { mutableStateOf(true) }
+        Switch(
+            checked = checked,
+            onCheckedChange = {checked = it},
+        )
+        var checked2 by remember{ mutableStateOf(true) }
+        Switch(
+            checked = checked2,
+            onCheckedChange = {checked2 = it},
+            //adding an icon that will show only when the switch is active
+            thumbContent = if (checked2){
+                {
+                    Icon(
+                        Icons.Filled.Check,
+                        contentDescription = "Check Switch",
+                        Modifier.size(InputChipDefaults.AvatarSize)
+                    )
+                }
+            }else {
+                null
+            }
+        )
+        var checked3 by remember { mutableStateOf(true) }
+        Checkbox(
+            checked = checked3,
+            onCheckedChange = {checked3 = it}
+        )
     }
 }
