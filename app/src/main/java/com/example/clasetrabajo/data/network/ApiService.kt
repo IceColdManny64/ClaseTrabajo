5509b2ed578b17cdc31abb4b62b558049c2be6b4
JOSÉ MANUEL JUÁREZ DÍAZ <22151192@aguascalientes.tecnm.mx>
2025-04-02 08:00:43 -0600

    Added elements (model, ApiService.kt, view model) to call a different API and request a list of accounts in AccountsScreen.kt

--- app/src/main/java/com/example/clasetrabajo/data/network/ApiService.kt
+++ app/src/main/java/com/example/clasetrabajo/data/network/ApiService.kt
@@ -1,12 +1,17 @@
     package com.example.clasetrabajo.data.network
 
+    import com.example.clasetrabajo.data.model.AccountModel
     import com.example.clasetrabajo.data.model.UserModel
     import retrofit2.http.POST
     import com.google.gson.JsonObject
     import retrofit2.Response
     import retrofit2.http.Body
+    import retrofit2.http.GET
 
     interface ApiService {
         @POST("user")
         suspend fun login(@Body username: UserModel):Response<JsonObject>
+
+        @GET("service")
+        suspend fun getAccounts():Response<List<AccountModel>>
     }
--- app/src/main/java/com/example/clasetrabajo/ui/screens/AccountsScreen.kt
+++ app/src/main/java/com/example/clasetrabajo/ui/screens/AccountsScreen.kt
@@ -1,14 +1,39 @@
 package com.example.clasetrabajo.ui.screens
 
+import android.util.Log
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
+import com.example.clasetrabajo.data.model.AccountModel
+import com.example.clasetrabajo.data.viewmodel.AccountViewModel
+import com.example.clasetrabajo.ui.components.AccountCardComponent
 import com.example.clasetrabajo.ui.components.TopBarComponent
 
 @Composable
-fun AccountsScreen(navController: NavHostController){
+fun AccountsScreen(
+    navController: NavHostController,
+    viewModel: AccountViewModel = viewModel()
+){
+    var accounts by remember { mutableStateOf<List<AccountModel>>(emptyList()) }
     Column(){
         TopBarComponent(title = "Account Screen")
+        //Launched effect is used to show animations and other things, in this case it will
+        //be used to call the api and manage the interface updates.
+        LaunchedEffect(Unit) {
+            viewModel.getAccounts { response ->
+                if(response.isSuccessful){
+                    accounts = response.body() ?: emptyList()
+                } else {
+                    Log.d("debug", "Failed to load accounts: ${response.code()}")
+                }
+            }
+        }
+        AccountCardComponent(1, "Name", "Username", "")
     }
-
 }
